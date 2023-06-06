22a0bc92e938745388a264d84208013f9042effa
MaquiDuran2807 <111880328+MaquiDuran2807@users.noreply.github.com>
2023-06-06 17:09:07 -0500

    Update settings.py

--- codensolar/settings.py
+++ codensolar/settings.py
@@ -157,7 +157,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS=[(os.path.join(BASE_DIR,"statics"))]
 MEDIA_URL='/media/'
